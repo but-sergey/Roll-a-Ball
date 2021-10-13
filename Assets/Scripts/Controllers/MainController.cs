134467ce19a830be215906950430d41c69f3dc4f
but-sergey <but-sergey@bk.ru>
2021-10-13 21:32:01 +0300

    Вынес генератор карты в отдельный класс

--- Assets/Scripts/Controllers/MainController.cs
+++ Assets/Scripts/Controllers/MainController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 namespace GB.Controllers
@@ -12,9 +11,14 @@ namespace GB.Controllers
         [SerializeField] private GameObject _floorPrefab;
         [SerializeField] private GameObject _playerPrefab;
         [SerializeField] private GameObject _goodBonusPrefab;
+        [SerializeField] private int _rows = 49;
+        [SerializeField] private int _cols = 49;
+        [SerializeField] private int _goodBonuses = 25;
+        [SerializeField] private int _badBonuses = 5;
 
         private GameObject _floor;
         private GameObject _player;
+        private MapGenerator _map;
         private readonly float _planeScale = 10.0f;
 
         #endregion
@@ -24,11 +28,11 @@ namespace GB.Controllers
 
         private void Start()
         {
-            GenerateMap();
+            _map = new MapGenerator(_rows, _cols, _goodBonuses, _badBonuses);
 
             CreateFloor();
 
-            GenerateLevel(Program.map);
+            GenerateLevel(_map);
 
             CreatePlayer();
         }
@@ -38,17 +42,10 @@ namespace GB.Controllers
 
         #region Methods
 
-        private void GenerateMap()
-        {
-            Program.ClearMap(ref Program.map);
-            Program.RemoveWall(ref Program.map);
-            Program.GenerateGoodBouses(ref Program.map, 25);
-        }
-
         private void CreateFloor()
         {
             _floor = Instantiate(_floorPrefab, Vector3.zero, Quaternion.identity);
-            SetFloorDimensions(_floor, _wallPrefab.transform.localScale, Program.map.GetLength(0), Program.map.GetLength(1));
+            SetFloorDimensions(_floor, _wallPrefab.transform.localScale, _map.Rows, _map.Cols);
         }
 
         private void CreatePlayer()
@@ -72,20 +69,20 @@ namespace GB.Controllers
             floor.transform.position = new Vector3(x, y, z);
         }
 
-        private void GenerateLevel(genCell[,] map)
+        private void GenerateLevel(MapGenerator map)
         {
             float x = 0.0f;
             float y = 0.5f * _wallPrefab.transform.localScale.y;
             float z = 0.0f;
 
-            for (var i = 0; i < map.GetLength(0); i++)
+            for (var i = 0; i < map.Rows; i++)
             {
-                for (var j = 0; j < map.GetLength(1); j++)
+                for (var j = 0; j < map.Cols; j++)
                 {
                     x = i * _wallPrefab.transform.localScale.x;
                     z = j * _wallPrefab.transform.localScale.z;
 
-                    switch (map[i, j].Value)
+                    switch (map.Map[i, j].Value)
                     {
                         case -1:
                             Instantiate(_wallPrefab, new Vector3(x, y, z), Quaternion.identity);
@@ -100,154 +97,5 @@ namespace GB.Controllers
 
         #endregion
 
-
-        #region Govnokod
-
-        struct genCell
-        {
-            public int Row { get; set; }
-            public int Col { get; set; }
-            public bool Visited { get; set; }
-            public int Value { get; set; }
-        }
-
-        static class Program
-        {
-            static public int Rows = 49;
-            static public int Cols = 49;
-            static public System.Random rand = new System.Random();
-
-            static public genCell[,] map = new genCell[Rows, Cols];
-
-            static public void ClearMap(ref genCell[,] M)
-            {
-                for (var i = 0; i < M.GetLength(0); i++)
-                {
-                    for (var j = 0; j < M.GetLength(1); j++)
-                    {
-                        if ((i % 2 != 0 && j % 2 != 0) && (i < Rows - 1))
-                        {
-                            M[i, j].Value = 0;
-                        }
-                        else
-                        {
-                            M[i, j].Value = -1;
-                        }
-                        M[i, j].Row = i;
-                        M[i, j].Col = j;
-                        M[i, j].Visited = false;
-                    }
-                }
-            }
-
-            static public void RemoveWall(ref genCell[,] M)
-            {
-                genCell current = M[1, 1];
-                current.Visited = true;
-
-                Stack<genCell> stack = new Stack<genCell>();
-
-                do
-                {
-                    List<genCell> cells = new List<genCell>();
-
-                    int row = current.Row;
-                    int col = current.Col;
-
-                    if (row - 1 > 0 && !M[row - 2, col].Visited) cells.Add(M[row - 2, col]);
-                    if (col - 1 > 0 && !M[row, col - 2].Visited) cells.Add(M[row, col - 2]);
-
-                    if (row < Rows - 3 && !M[row + 2, col].Visited) cells.Add(M[row + 2, col]);
-                    if (col < Cols - 3 && !M[row, col + 2].Visited) cells.Add(M[row, col + 2]);
-
-                    if (cells.Count > 0)
-                    {
-                        genCell selected = cells[Random.Range(0, cells.Count)];
-                        RemoveCurrentWall(ref M, current, selected);
-
-                        selected.Visited = true;
-                        M[selected.Row, selected.Col].Visited = true;
-                        stack.Push(selected);
-                        current = selected;
-                    }
-                    else
-                    {
-                        current = stack.Pop();
-                    }
-
-                } while (stack.Count > 0);
-            }
-
-            static public void RemoveCurrentWall(ref genCell[,] M, genCell current, genCell selected)
-            {
-                if (current.Row == selected.Row)
-                {
-                    if (current.Col > selected.Col)
-                    {
-                        M[current.Row, current.Col - 1].Value = 0;
-                    }
-                    else
-                    {
-                        M[selected.Row, selected.Col - 1].Value = 0;
-                    }
-                }
-                else
-                {
-                    if (current.Row > selected.Row)
-                    {
-                        M[current.Row - 1, current.Col].Value = 0;
-                    }
-                    else
-                    {
-                        M[selected.Row - 1, selected.Col].Value = 0;
-                    }
-                }
-            }
-
-            static public void GenerateGoodBouses(ref genCell[,] M, int count)
-            {
-                for(var i = 0; i < count; i++)
-                {
-                    var x = Random.Range(0, M.GetLength(0));
-                    var y = Random.Range(0, M.GetLength(1));
-
-                    if(M[x,y].Value == 0)
-                    {
-                        PlaceBonus(ref M, x, y, 1);
-                        continue;
-                    }
-                    else
-                    {
-                        var saved_x = x;
-                        var saved_y = y;
-                        do
-                        {
-                            y++;
-                            if (y >= M.GetLength(1))
-                            {
-                                y = 0;
-                                x++;
-                                if(x >= M.GetLength(1))
-                                {
-                                    x = 0;
-                                }
-                            }
-                        } while ((M[x, y].Value != 0) || ((x == saved_x) && (y == saved_y)));
-
-                        if(M[x,y].Value == 0)
-                        {
-                            PlaceBonus(ref M, x, y, 1);
-                        }
-                    }
-                }
-            }
-
-            static void PlaceBonus(ref genCell[,] M, int x, int y, int type)
-            {
-                M[x, y].Value = type;
-            }
-        }
-
-        #endregion
     }
 }
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,155 @@
+﻿using GB.Structs;
+using System;
+using System.Collections.Generic;
+
+namespace GB
+{
+    public class MapGenerator
+    {
+        public int Rows { get; private set; }
+        public int Cols { get; private set; }
+        public int GoodBonuses { get; private set; }
+        public int BadBonuses { get; private set; }
+        public genCell[,] Map;
+        private System.Random rand = new System.Random();
+
+        public MapGenerator(int Rows, int Cols, int GoodBonuses, int BadBonuses)
+        {
+            this.Rows = Rows;
+            this.Cols = Cols;
+            Map = (genCell[,]) Array.CreateInstance(typeof(genCell), new int[] { Rows, Cols });
+            ClearMap(ref Map);
+            RemoveWall(ref Map);
+            GenerateGoodBouses(ref Map, GoodBonuses);
+        }
+
+        private void ClearMap(ref genCell[,] M)
+        {
+            for (var i = 0; i < M.GetLength(0); i++)
+            {
+                for (var j = 0; j < M.GetLength(1); j++)
+                {
+                    if ((i % 2 != 0 && j % 2 != 0) && (i < Rows - 1))
+                    {
+                        M[i, j].Value = 0;
+                    }
+                    else
+                    {
+                        M[i, j].Value = -1;
+                    }
+                    M[i, j].Row = i;
+                    M[i, j].Col = j;
+                    M[i, j].Visited = false;
+                }
+            }
+        }
+
+        private void RemoveWall(ref genCell[,] M)
+        {
+            genCell current = M[1, 1];
+            current.Visited = true;
+
+            Stack<genCell> stack = new Stack<genCell>();
+
+            do
+            {
+                List<genCell> cells = new List<genCell>();
+
+                int row = current.Row;
+                int col = current.Col;
+
+                if (row - 1 > 0 && !M[row - 2, col].Visited) cells.Add(M[row - 2, col]);
+                if (col - 1 > 0 && !M[row, col - 2].Visited) cells.Add(M[row, col - 2]);
+
+                if (row < Rows - 3 && !M[row + 2, col].Visited) cells.Add(M[row + 2, col]);
+                if (col < Cols - 3 && !M[row, col + 2].Visited) cells.Add(M[row, col + 2]);
+
+                if (cells.Count > 0)
+                {
+                    genCell selected = cells[rand.Next(cells.Count)];
+                    RemoveCurrentWall(ref M, current, selected);
+
+                    selected.Visited = true;
+                    M[selected.Row, selected.Col].Visited = true;
+                    stack.Push(selected);
+                    current = selected;
+                }
+                else
+                {
+                    current = stack.Pop();
+                }
+
+            } while (stack.Count > 0);
+        }
+
+        private void RemoveCurrentWall(ref genCell[,] M, genCell current, genCell selected)
+        {
+            if (current.Row == selected.Row)
+            {
+                if (current.Col > selected.Col)
+                {
+                    M[current.Row, current.Col - 1].Value = 0;
+                }
+                else
+                {
+                    M[selected.Row, selected.Col - 1].Value = 0;
+                }
+            }
+            else
+            {
+                if (current.Row > selected.Row)
+                {
+                    M[current.Row - 1, current.Col].Value = 0;
+                }
+                else
+                {
+                    M[selected.Row - 1, selected.Col].Value = 0;
+                }
+            }
+        }
+
+        private void GenerateGoodBouses(ref genCell[,] M, int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                var x = rand.Next(M.GetLength(0));
+                var y = rand.Next(M.GetLength(1));
+
+                if (M[x, y].Value == 0)
+                {
+                    PlaceBonus(ref M, x, y, 1);
+                    continue;
+                }
+                else
+                {
+                    var saved_x = x;
+                    var saved_y = y;
+                    do
+                    {
+                        y++;
+                        if (y >= M.GetLength(1))
+                        {
+                            y = 0;
+                            x++;
+                            if (x >= M.GetLength(1))
+                            {
+                                x = 0;
+                            }
+                        }
+                    } while ((M[x, y].Value != 0) || ((x == saved_x) && (y == saved_y)));
+
+                    if (M[x, y].Value == 0)
+                    {
+                        PlaceBonus(ref M, x, y, 1);
+                    }
+                }
+            }
+        }
+
+        private void PlaceBonus(ref genCell[,] M, int x, int y, int type)
+        {
+            M[x, y].Value = type;
+        }
+ 
+    }
+}
