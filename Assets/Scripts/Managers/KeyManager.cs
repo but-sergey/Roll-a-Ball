d23108d804231e931bd4332c00184bbbede2256f
but-sergey <but-sergey@bk.ru>
2021-11-14 15:39:04 +0300

    KeyManager

---
+++ Assets/Scripts/Managers/KeyManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace RollABall
+{
+    public static class KeyManager
+    {
+        public const KeyCode SAVE = (KeyCode.RightControl | KeyCode.LeftControl) & KeyCode.S;
+        public const KeyCode LOAD = (KeyCode.RightControl | KeyCode.LeftControl) & KeyCode.L;
+    }
+}
