b49c7e1d4c700b1aa77af93667c4398d5e280ec8
but-sergey <but-sergey@bk.ru>
2021-11-14 16:30:29 +0300

    Система сохранения данных (на базе Json)

--- Assets/Scripts/Managers/KeyManager.cs
+++ Assets/Scripts/Managers/KeyManager.cs
@@ -4,7 +4,7 @@ namespace RollABall
 {
     public static class KeyManager
     {
-        public const KeyCode SAVE = (KeyCode.RightControl | KeyCode.LeftControl) & KeyCode.S;
-        public const KeyCode LOAD = (KeyCode.RightControl | KeyCode.LeftControl) & KeyCode.L;
+        public const KeyCode SAVE = KeyCode.S;
+        public const KeyCode LOAD = KeyCode.L;
     }
 }
